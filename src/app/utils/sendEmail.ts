84d6c5406342c27a174dcbd66bf42340980efb98
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-09-12 10:37:38 +0600

    transation update

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,4 +1,13 @@
-export const sendEmail=()=>{
-    console.log("hello smtp");
+ import nodemailer from 'nodemailer'
+import { envVars } from '../config/envVars';
+ 
+ const transporter = nodemailer.createTransport({
     
-}
+     secure:true,
+     auth:{
+        user:envVars.SMTP_USER,
+        pass:envVars.SMTP_PASS,
+     }
+    host:envVars.SMTP_HOST,
+     port:Number(envVars.SMTP_PORT),
+  });

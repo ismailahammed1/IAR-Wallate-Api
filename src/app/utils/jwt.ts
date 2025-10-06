f976f1c1c740c3097cd3031dc3a106f485c25394
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-10-06 20:38:03 +0600

    token

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken"
+import { AppError } from "../errorHelpers/AppError";
 
 export const generateToken = (payload: JwtPayload, secret: string, expiresIn: string) => {
     const token = jwt.sign(payload, secret, {
@@ -8,9 +10,12 @@ export const generateToken = (payload: JwtPayload, secret: string, expiresIn: st
     return token
 }
 
-export const verifyToken = (token: string, secret: string) => {
-
-    const verifiedToken = jwt.verify(token, secret);
-
-    return verifiedToken
-}
+export const verifyToken = (token: string, secret: string): JwtPayload => {
+  try {
+        const decoded = jwt.verify(token, secret) as JwtPayload;
+    return decoded;
+  } catch (err: any) {
+    console.error("Token verification failed:", err.message);
+    throw new AppError(401, "Invalid or expired token");
+  }
+};
